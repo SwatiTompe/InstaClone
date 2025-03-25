a06d53b9984621b34191976bf11581e39c8389fd
Swati T <swatiptompe@gmail.com>
2025-03-25 17:52:20 +0530

    commiting my first swiftUI project step 1

---
+++ InstaClone/InstaClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaClone
+//
+//  Created by Techizer Mac02 on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaClone/InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Techizer Mac02 on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
